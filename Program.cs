c95338a2ce571b6b89c51592360ee9ea8c353ee3
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-03 17:43:52 -0300

    FatorialSimples

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Exercício do site URI "Fatorial Simples"
+// Nesse programa vamos calcular o fatorial do número inteiro "N" digitado.
+
+
+int N = int.Parse(Console.ReadLine());
+int F = 1;
+for (int i = 1; i <= N; i ++)
+{
+    F = F * i;
+}
+Console.WriteLine(F);
